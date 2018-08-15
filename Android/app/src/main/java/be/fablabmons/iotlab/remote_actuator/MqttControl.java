3b74f72eb61520e75876d5ecf62cb3d351304dea
François Roland <fr.roland@gmail.com>
2018-08-15 15:49:32 +0200

    Provide default configuration

--- Android/app/src/main/java/be/fablabmons/iotlab/remote_actuator/MqttControl.java
+++ Android/app/src/main/java/be/fablabmons/iotlab/remote_actuator/MqttControl.java
@@ -8,7 +8,7 @@ class MqttControl implements LightControl {
     private static final String TAG = "MqttControl";
     private static final String BROKER_CONNECT_STRING = "tcp://10.130.1.204:1883";
     private static final String TOPIC = STUDENT_ID + "/remote-actuator/light";
-    private static final String CLIENT_ID = STUDENT_ID;
+    private static final String CLIENT_ID = STUDENT_ID + "-android";
 
     @Override
     public void turnOn() {
--- Arduino/iotlab-remote-actuator/iotlab-remote-actuator.ino
+++ Arduino/iotlab-remote-actuator/iotlab-remote-actuator.ino
@@ -1,6 +1,11 @@
 #include <BridgeClient.h>
 #include <PubSubClient.h>
 
+const char* broker = "10.130.1.204";
+const int port = 1883;
+const char* topic = "studentXX/remote-actuator/light";
+const char* mqttClientId = "studentXX-arduino";
+
 void setup() {
   // put your setup code here, to run once:
 
