cfe9ea28a133a8341b0b04fea4a9ce6b1d6fbda1
François Roland <fr.roland@gmail.com>
2018-08-15 15:34:37 +0200

    Write Arduino template

---
+++ Arduino/iotlab-remote-actuator/iotlab-remote-actuator.ino
@@ -0,0 +1,12 @@
+#include <BridgeClient.h>
+#include <PubSubClient.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
