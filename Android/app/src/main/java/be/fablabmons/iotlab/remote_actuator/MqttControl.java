c975abe5b21e60f3abd5c1ead4df2e76dc5bd530
François Roland <fr.roland@gmail.com>
2018-08-21 14:47:55 +0200

    Write solution

--- Android/app/src/main/java/be/fablabmons/iotlab/remote_actuator/MqttControl.java
+++ Android/app/src/main/java/be/fablabmons/iotlab/remote_actuator/MqttControl.java
@@ -3,60 +3,44 @@ package be.fablabmons.iotlab.remote_actuator;
 import android.util.Log;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 class MqttControl implements LightControl {
-    private static final String STUDENT_ID = "teacher"; // TODO change to unique ID
+    private static final String STUDENT_ID = "teacher";
 
     private static final String TAG = "MqttControl";
     private static final String BROKER_CONNECT_STRING = "tcp://10.10.32.169:1883";
     private static final String TOPIC = STUDENT_ID + "/remote-actuator/light";
     private static final String CLIENT_ID = STUDENT_ID + "-android";
-
-    private MqttClientPersistence persistence = new MemoryPersistence();
+    private MemoryPersistence persistence = new MemoryPersistence();
 
     @Override
     public void turnOn() {
         Log.i(TAG, "turn on the light");
-        tryToSendMessage("ON");
+        sendMessage("ON");
     }
 
-    private void tryToSendMessage(String message) {
+    private void sendMessage(String msgString) {
         try {
-            sendMessage(message);
+            MqttClient sampleClient = new MqttClient(BROKER_CONNECT_STRING, CLIENT_ID, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            sampleClient.connect(connOpts);
+            MqttMessage message = new MqttMessage(msgString.getBytes());
+            message.setQos(0);
+            sampleClient.publish(TOPIC, message);
+            sampleClient.disconnect();
         } catch (MqttException e) {
-            Log.i(TAG, "turnOn: MQTT exception", e);
+            Log.i(TAG, "turnOn: MQTT error", e);
         }
-    }
-
-    private void sendMessage(String message) throws MqttException {
-        MqttClient mqttClient = connectClient();
-        publishMessage(mqttClient, message);
-        mqttClient.disconnect();
-    }
-
-    private MqttClient connectClient() throws MqttException {
-        MqttClient mqttClient = new MqttClient(BROKER_CONNECT_STRING, CLIENT_ID, persistence);
-        MqttConnectOptions connectOptions = new MqttConnectOptions();
-        connectOptions.setCleanSession(true);
-        mqttClient.connect(connectOptions);
-        return mqttClient;
-    }
-
-    private void publishMessage(MqttClient mqttClient, String msgString) throws MqttException {
-        MqttMessage message = new MqttMessage(msgString.getBytes());
-        message.setQos(0);
-        message.setRetained(true);
-        mqttClient.publish(TOPIC, message);
     }
 
     @Override
     public void turnOff() {
         Log.i(TAG, "turn off the light");
-        tryToSendMessage("OFF");
+        sendMessage("OFF");
     }
 }
--- Arduino/iotlab-remote-actuator/iotlab-remote-actuator.ino
+++ Arduino/iotlab-remote-actuator/iotlab-remote-actuator.ino
@@ -1,17 +1,55 @@
 #include <BridgeClient.h>
 #include <PubSubClient.h>
 
-const char* broker = "10.130.1.204";
+const char* broker = "10.10.32.169";
 const int port = 1883;
-const char* topic = "studentXX/remote-actuator/light";
-const char* mqttClientId = "studentXX-arduino";
+const char* topic = "teacher/remote-actuator/light";
+const char* mqttClientId = "teacher-arduino";
+
+const int ledPin = 3;
+
+BridgeClient bridgeClient;
+PubSubClient pubSubClient;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(2000);
 
+  Serial.println("Setup START");
+  
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
+  Bridge.begin();
+  
+  pubSubClient.setServer(broker, port);
+  pubSubClient.setClient(bridgeClient);
+  pubSubClient.setCallback(callback);
+  
+  pubSubClient.connect(mqttClientId);
+
+  pubSubClient.subscribe(topic);
+
+  Serial.println("Setup END");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.println("Callback START");
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message = message + ((char)payload[i]);
+  }
+  Serial.println(message);
+  if (message == "ON") {
+    Serial.println("Led ON");
+    digitalWrite(ledPin, HIGH);
+  } else {
+    Serial.println("Led OFF");
+    digitalWrite(ledPin, LOW);
+  }
+  Serial.println("Callback END");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  pubSubClient.loop();
 }
